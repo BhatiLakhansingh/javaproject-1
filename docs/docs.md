1c57a2cd4c1ade90609d7010724a6eb7502222f2
BHATTILAKHANSINGH <jaswanthsinghjassi@gmail.com>
2023-02-07 00:38:39 +0530

    sprint-3 work started

--- docs/docs.md
+++ docs/docs.md
@@ -1,1 +1,2 @@
 added some docs to sprint-2
+sprint-3 fetaures listed
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 sprint-1 coding started
 imp bug fix has been done in sprint-1
 sprint-2 coding is started
-
 imp feauture added in sprint-2
+sprint-3 imp features to be added
+5fd7f7c (sprint-3 work started)
